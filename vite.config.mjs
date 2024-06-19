e18ad777b412086122089ec7f2d0b54d149fc142
Martin <mschonberger11@gmail.com>
2024-06-19 17:26:43 -0300

    Cambios en la configuracion

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/FrontEnd-Pasteleria/', // Ajusta esto según la estructura de tu repositorio
-  build: {
-    outDir: 'dist', // Carpeta de salida donde se generan los archivos optimizados
-  },
+  base: '/FrontEnd-Pasteleria/', // Ajusta esto según el nombre de tu repositorio en GitHub
 });
