1d53dab4dbd0545bbd694b9b4e79b24f1593c12b
Martin <mschonberger11@gmail.com>
2024-06-19 16:44:47 -0300

    Se hicieron cambios en las redirecciones

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/FrontEnd-Pasteleria/', // El nombre de tu repositorio en GitHub
+plugins: [vue()],
+base: '/FrontEnd-Pasteleria/', // El nombre de tu repositorio en GitHub
 });
