b92b10159f65da6fe04e35e520033f51917f4cac
MartinSch11 <mschonberger11@gmail.com>
2024-07-01 23:45:13 -0300

    Cambio de imagen

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -7,7 +7,12 @@
       :class="[' fixed top-0 right-0 h-full w-80 bg-white shadow-lg z-50 overflow-y-auto transition-transform transform', { 'translate-x-0': isCartOpen, 'translate-x-full': !isCartOpen }]">
       <template v-if="items.length === 0">
         <div class="flex flex-col items-center justify-center h-full">
-          <img src="../assets/imagen/icons/shopping-bag-black.svg" alt="Empty Cart" class="w-12 h-auto mb-4">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-16 h-16 mb-4 text-gray-600" fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+          </svg>
+
           <p class="text-lg text-gray-600">Tu carrito está vacío</p>
         </div>
       </template>
