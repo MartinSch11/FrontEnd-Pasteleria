d1f52b060281a9172e8150a973536bd65e0d3c54
MartinSch11 <mschonberger11@gmail.com>
2024-07-12 17:28:59 -0300

    redirecciones

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,10 +26,10 @@
             ¡Explorá!
           </h4>
           <ul class="list-none p-0 m-0">
-            <li><a href="/Home" class=" hover:text-[#FAF298] transition-colors">Inicio</a></li>
-            <li><a href="/Productos" class=" hover:text-[#FAF298] transition-colors">Productos</a></li>
-            <li><a href="/Nosotros" class=" hover:text-[#FAF298] transition-colors">Nosotros</a></li>
-            <li><a href="/Contacto" class=" hover:text-[#FAF298] transition-colors">Contacto</a></li>
+            <li><router-link to="/Home" class="hover:text-[#FAF298] transition-colors">Inicio</router-link> </li>
+            <li><router-link to="/Productos" class="hover:text-[#FAF298] transition-colors">Productos</router-link> </li>
+            <li><router-link to="/Nosotros" class="hover:text-[#FAF298] transition-colors">Nosotros</router-link> </li>
+            <li><router-link to="/Contacto" class="hover:text-[#FAF298] transition-colors">Contacto</router-link> </li>
           </ul>
         </div>
         <div class="w-1/4 px-4 mb-6 lg:mb-0 lg:text-base text-xs">
